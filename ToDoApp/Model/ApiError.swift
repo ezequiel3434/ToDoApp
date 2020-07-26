87f61e3526ff99d9987cee4c0002d201e9ea316a
Ezequiel <ezequiel3434@gmail.com>
2020-07-26 15:07:46 -0300

    modals

---
+++ ToDoApp/Model/ApiError.swift
@@ -0,0 +1,13 @@
+//
+//  ApiError.swift
+//  ToDoApp
+//
+//  Created by Ezequiel Parada Beltran on 26/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import Foundation
+
+struct ApiError: Codable {
+    let message: String
+}
---
+++ ToDoApp/Model/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoApp
+//
+//  Created by Ezequiel Parada Beltran on 26/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import Foundation
+struct Todo: Codable {
+    let item: String
+    let priority: Int
+}
